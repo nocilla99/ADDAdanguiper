674fb3514aafa270782c440452f576dee20341d1
nocilla99 <47306220+nocilla99@users.noreply.github.com>
2022-04-29 04:00:19 +0200

    Update Ej2_Edge.java

--- Java/PI6_danguiper/src/ejercicio1/Ej2_Edge.java
+++ Java/PI6_danguiper/src/ejercicio1/Ej2_Edge.java
@@ -2,10 +2,10 @@ package ejercicio1;
 
 import us.lsi.graphs.virtual.SimpleEdgeAction;
 
-public record Ej2_Edge(Ej2_Vertex source, Ej2_Vertex target, Integer action,Double weight) implements SimpleEdgeAction<Ej2_Vertex,Integer>{
+public record Ej1_Edge(Ej1_Vertex source, Ej1_Vertex target, Integer action,Double weight) implements SimpleEdgeAction<Ej2_Vertex,Integer>{
 
-		public static Ej2_Edge of(Ej2_Vertex source, Ej2_Vertex target, Integer action) {
-			return new Ej2_Edge(source,target,action,(action==-1)?0.:1.);
+		public static Ej1_Edge of(Ej1_Vertex source, Ej1_Vertex target, Integer action) {
+			return new Ej1_Edge(source,target,action,(action==-1)?0.:1.);
 			
 		}
 
