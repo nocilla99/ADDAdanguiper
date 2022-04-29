a82d4108c64607bc2fcf8aba280b1f874d0df0ce
nocilla99 <47306220+nocilla99@users.noreply.github.com>
2022-04-29 03:56:02 +0200

    Update testPDR1.java

--- Java/PI6_danguiper/src/ejercicio1/testPDR1.java
+++ Java/PI6_danguiper/src/ejercicio1/testPDR1.java
@@ -12,16 +12,16 @@ public class testPDR1 {
 	public static void main(String[] args) {
 		datosEj1.iniDatos("./fichero/PI6Ej1DatosEntrada2.txt");
 		
-		Ej2_Vertex v1= Ej2_Vertex.V_inicial();
+		Ej1_Vertex v1= Ej1_Vertex.V_inicial();
 		
 		
-		EGraph<Ej2_Vertex, Ej2_Edge> grafo= 
-				SimpleVirtualGraph.sum(v1,Ej2_Vertex.goal(),e-> (double)e.weight());
+		EGraph<Ej1_Vertex, Ej1_Edge> grafo= 
+				SimpleVirtualGraph.sum(v1,Ej1_Vertex.goal(),e-> (double)e.weight());
 		
-		DynamicProgrammingReduction<Ej2_Vertex, Ej2_Edge> dpr= 
+		DynamicProgrammingReduction<Ej1_Vertex, Ej1_Edge> dpr= 
 				DynamicProgrammingReduction.of(grafo,heuristicaEj1::heuristic,PDType.Max);
 		
-		GraphPath<Ej2_Vertex,Ej2_Edge> S= dpr.search().get();
+		GraphPath<Ej1_Vertex,Ej1_Edge> S= dpr.search().get();
 		Solucion1 sol= Solucion1.of(S);
 		System.out.println(sol);
 		
