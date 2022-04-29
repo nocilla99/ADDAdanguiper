d1ffd647f6b5b08f8fc9aef434193b9bcfc152f5
nocilla99 <47306220+nocilla99@users.noreply.github.com>
2022-04-29 03:57:07 +0200

    Update testBT1.java

--- Java/PI6_danguiper/src/ejercicio1/testBT1.java
+++ Java/PI6_danguiper/src/ejercicio1/testBT1.java
@@ -10,15 +10,15 @@ public class testBT1 {
 	public static void main(String[] args) {
 		datosEj1.iniDatos("./fichero/PI6Ej1DatosEntrada1.txt");
 		
-		Ej2_Vertex v1= Ej2_Vertex.V_inicial();
+		Ej1_Vertex v1= Ej1_Vertex.V_inicial();
 		
-		EGraph<Ej2_Vertex, Ej2_Edge> grafo= 
-				SimpleVirtualGraph.sum(v1,Ej2_Vertex.goal(),e-> (double)e.weight());
+		EGraph<Ej1_Vertex, Ej1_Edge> grafo= 
+				SimpleVirtualGraph.sum(v1,Ej1_Vertex.goal(),e-> (double)e.weight());
 		
-		BackTracking<Ej2_Vertex, Ej2_Edge, Integer> bt1=
+		BackTracking<Ej1_Vertex, Ej1_Edge, Integer> bt1=
 				BackTracking.of(grafo,heuristicaEj1::heuristic,null, BTType.Max);
 															   //greedy?
-		GraphPath<Ej2_Vertex,Ej2_Edge> S= bt1.optimalPath().get();
+		GraphPath<Ej1_Vertex,Ej1_Edge> S= bt1.optimalPath().get();
 		Solucion1 sol= Solucion1.of(S);
 		System.out.println(sol);
 	}
