cc5b4bee3b578f32be4e94cc5a2edd27912a6f75
nocilla99 <47306220+nocilla99@users.noreply.github.com>
2022-04-29 03:58:04 +0200

    Update testAStar1.java

--- Java/PI6_danguiper/src/ejercicio1/testAStar1.java
+++ Java/PI6_danguiper/src/ejercicio1/testAStar1.java
@@ -12,15 +12,15 @@ public class testAStar1 {
 	public static void main(String[] args) {
 		datosEj1.iniDatos("./fichero/PI6Ej1DatosEntrada2.txt");
 		
-		Ej2_Vertex v1= Ej2_Vertex.V_inicial();
+		Ej2_Vertex v1= Ej1_Vertex.V_inicial();
 		
 		
-		EGraph<Ej2_Vertex, Ej2_Edge> graph= 
-				SimpleVirtualGraph.sum(v1,Ej2_Vertex.goal(),e-> (double)e.weight());
+		EGraph<Ej1_Vertex, Ej1_Edge> graph= 
+				SimpleVirtualGraph.sum(v1,Ej1_Vertex.goal(),e-> (double)e.weight());
 		
-		AStar<Ej2_Vertex, Ej2_Edge> ms= AStar.of(graph,heuristicaEj1::heuristic,AStarType.Max);
+		AStar<Ej1_Vertex, Ej1_Edge> ms= AStar.of(graph,heuristicaEj1::heuristic,AStarType.Max);
 		
-		GraphPath<Ej2_Vertex, Ej2_Edge> path = ms.search().get();
+		GraphPath<Ej1_Vertex, Ej1_Edge> path = ms.search().get();
 		Solucion1 s =Solucion1.of(path);
 		System.out.println(s);
 		
